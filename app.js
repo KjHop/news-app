ce429c0ff2977a4f1a9c83676999745687081d95
[KjHop] <[kjhop@wp.pl]>
2019-04-18 18:11:11 +0200

    Getting and changing sources

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 window.addEventListener('load', e =>{
-    updateNews();
+    const defaultSource = "bbc-news";
+    updateNews(defaultSource);
+    updateSources();
 });
 
-async function updateNews(){
-    const result = await fetch("https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=124e6d7bc3ea499abcff822a5a9509e3");
+async function updateNews(source){
+    const result = await fetch(`https://newsapi.org/v2/everything?sources=${source}&apiKey=124e6d7bc3ea499abcff822a5a9509e3`);
     const json = await result.json();
     const news = json.articles.map(createNews).join("\n");
 
-    document.querySelector("main").innerHTML = news;
+    document.getElementById("news").innerHTML = news;
+}
+
+async function updateSources(){
+    const result = await fetch("https://newsapi.org/v2/sources?apiKey=124e6d7bc3ea499abcff822a5a9509e3");
+    const json = await result.json();
+    const sources = json.sources.map(source=>{
+        return `<option onclick='updateNews("${source.id}")'>${source.name}</option>`
+    }).join("\n");
+
+    document.getElementById('sourceSelector').innerHTML = await sources;
 }
 
 function createNews(article){
@@ -16,7 +28,7 @@ function createNews(article){
             <h2>${article.title}</h2>
             <h3>${article.description}</h3>
             <img src='${article.urlToImage}'/>
-            <p>${article.content}</p>
+            <p>${article.content} <a href='${article.url}'>Read more...</a></p>
         </div>
     `;
 }
